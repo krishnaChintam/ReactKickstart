8dd2afc73e24cafad53813696dea23443338d912
dsv111 <srinivas.dasari2019@gmail.com>
2025-08-31 23:19:10 +0530

    Corrected the mobile theam and Ag Grid

--- src/pages/tempPage/tempPage.jsx
+++ src/pages/tempPage/tempPage.jsx
@@ -1,12 +1,45 @@
-
+import React from 'react'
+import DataTable from '../../components/common/DataTable'
 
 const TempPage = () => {
+  const columns = [
+    { field: 'id', headerName: 'ID', minWidth: 70 },
+    { field: 'firstName', headerName: 'First name', minWidth: 130 },
+    { field: 'lastName', headerName: 'Last name', minWidth: 130 },
+    { field: 'age', headerName: 'Age', type: 'number', minWidth: 90 },
+  ]
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 10, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 11, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 12, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 13, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 14, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 15, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 16, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 17, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 18, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 19, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ]
 
   return (
     <div>
-      <h1>Temporary Page</h1>   
+      <h1>Temporary Page</h1>
+      <DataTable
+        rowData={rows}
+        columnDefs={columns}
+      />
     </div>
-  );
-};
+  )
+}
 
-export default TempPage;
+export default TempPage
